e3458e6450900e027607d42278fb87fa5642ae3a
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-11-05 18:30:55 +0530

    on my own

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
 // import using require
-
+const Shape = require('./shape');
 // declare class
-
+class Circle extends Shape{
+    constructor(){
+        super();
+    }
+    calculateArea(){
+        console.log("Area of circle of color " + this.color + " is:");
+        return 3.14 * 2 * 2;
+    }
+}
 
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
-
+class Shape{
+    constructor(){
+        this.color = "green";
+    }
+    drawShape(){
+        console.log("draw Shape");
+    }
+    calculateArea(){
+        console.log("I need length and width to find area!!!");
+    }
+}
 // export class using module.exports
+module.exports = Shape;
